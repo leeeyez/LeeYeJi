e96a8e9b02cc224b75adae8744839fd7e2ae4f72
solux <lee.yeji0327@gmail.com>
2024-03-17 13:06:49 +0900

    js 추가

---
+++ week02/yeji.js
@@ -0,0 +1,11 @@
+alert("Welcome to yeji info page!");
+const mainbox = document.getElementById("mainbox");
+function darkmode() {
+    mainbox.style.backgroundColor = '#262626';
+    mainbox.style.color = 'white';
+}
+function lightmode() {
+    mainbox.style.backgroundColor = 'white';
+    mainbox.style.color = 'black';
+    h2.style.backgroundColor = 'cornsilk';
+}
